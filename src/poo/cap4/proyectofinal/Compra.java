dd863a67cf17819d637696d3fc53d3cc8231022d
jesuc <jesuc@DESKTOP-E5QD1MJ>
2021-09-12 15:20:25 -0500

    Creacion de Clases Vender Comprar y la CLase Padre

---
+++ src/poo/cap4/proyectofinal/Compra.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo.cap4.proyectofinal;
+
+/**
+ *
+ * @author jesuc
+ */
+public class Compra extends Registro{
+    
+    public Compra(String id, String nombre, int cantidad, double costoUnidad, double costoTotal) {
+        super(id, nombre, cantidad, costoUnidad, costoTotal);
+    }
+   
+    
+    
+}
---
+++ src/poo/cap4/proyectofinal/Venta.java
@@ -0,0 +1,14 @@
+
+package poo.cap4.proyectofinal;
+public class Venta extends Registro{
+    double ingresoTotal;
+
+    public Venta(String id, String nombre, int cantidad, double costoUnidad, double costoTotal) {
+        super(id, nombre, cantidad, costoUnidad, costoTotal);
+    }
+    
+    
+    void   movimientoEntrada(){
+        
+    }     
+}
